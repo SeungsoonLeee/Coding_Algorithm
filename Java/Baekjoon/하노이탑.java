1e02104f68849128cf9b40991e273c8668aceb6a
LSS <cjswosuny01@naver.com>
2019-07-26 17:06:30 +0900

    하노이탑 알고리즘 업로드(재귀함수, bufferedWriter)

---
+++ Java/Baekjoon/하노이탑.java
@@ -0,0 +1,33 @@
+package com.ssafy.step2.algo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 하노이탑 {
+	static int cnt = 0;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		cnt = (int) Math.pow(2, n) - 1;
+		bw.write(cnt + "\n");
+		hanoi(n, 1, 2, 3);
+		bw.flush();
+
+	}
+
+	public static void hanoi(int n, int from, int temp, int to) throws IOException {
+		if (n == 1) {
+			bw.write(from + " " + to + "\n");
+		} else {
+			hanoi(n - 1, from, to, temp);
+			bw.write(from + " " + to + "\n");
+			hanoi(n - 1, temp, from, to);
+		}
+	}
+}
