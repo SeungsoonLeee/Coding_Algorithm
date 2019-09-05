539c6957f52fe64e49f4f688edb6e916e86f8b67
LSS <cjswosuny01@naver.com>
2019-09-05 15:08:51 +0900

    BFS, 시뮬레이션

--- Java/Baekjoon/Main_16236_아기상어.java
+++ Java/Baekjoon/Main_16236_아기상어.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -13,15 +15,17 @@ public class Main_16236_아기상어 {
 	static int N, M, ans;
 	static int[][] map;
 	static boolean[][] visited;
+	static PriorityQueue<Integer> sizeList;
 	static int fishCnt;
 	static Shark babyShark;
-	static int[][] dir = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { -1, -0 } };
+	static int[][] dir = { { -1, 0 }, { 0, -1 }, { 0, 1 }, { 1, 0 } };
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
 		visited = new boolean[N][N];
+		sizeList = new PriorityQueue<Integer>();
 		ans = 0;
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,6 +33,7 @@ public class Main_16236_아기상어 {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				if (map[i][j] >= 1 && map[i][j] <= 6) {
 					fishCnt++;
+					sizeList.offer(map[i][j]);
 				}
 				if (map[i][j] == 9) {
 					babyShark = new Shark(i, j, 0);
@@ -37,39 +42,79 @@ public class Main_16236_아기상어 {
 		}
 
 		while (fishCnt > 0) {
+			int tempR = babyShark.r;
+			int tempC = babyShark.c;
+			if (!isPossible())
+				break;
 			for (int i = 0; i < visited.length; i++) {
 				Arrays.fill(visited[i], false);
 			}
 			move(babyShark.r, babyShark.c);
+			if(tempR == babyShark.r && tempC == babyShark.c) break;
 		}
 
 		System.out.println(ans);
 	}
 
+	private static boolean isPossible() {
+		
+		if (babyShark.size > sizeList.peek())
+			return true;
+		return false;
+	}
+
 	private static void move(int r, int c) {
 		Queue<Integer> queR = new LinkedList<Integer>();
 		Queue<Integer> queC = new LinkedList<Integer>();
+		ArrayList<Fish> eatFish = new ArrayList<Fish>();
 		visited[r][c] = true;
 		queR.offer(r);
 		queC.offer(c);
 
+		int distance = 0;
 		while (!queR.isEmpty()) {
-			int curR = queR.poll();
-			int curC = queC.poll();
-			for (int i = 0; i < 4; i++) {
-				int nextR = curR + dir[i][0];
-				int nextC = curR + dir[i][0];
-				if (nextR >= N || nextC >= N || nextC < 0 || nextC < 0 || map[nextR][nextC] > babyShark.size)
-					continue;
-				if (map[nextR][nextC] < babyShark.size) {
-					babyShark.cnt++;
-					babyShark.r = nextR;
-					babyShark.c = nextC;
-					fishCnt--;
-					return;
+			int size = queR.size();
+			boolean findMinDistance = false;
+			distance++;
+			while (size > 0) {
+				int curR = queR.poll();
+				int curC = queC.poll();
+				for (int i = 0; i < 4; i++) {
+					int nextR = curR + dir[i][0];
+					int nextC = curC + dir[i][1];
+					if (nextR >= N || nextC >= N || nextR < 0 || nextC < 0 || map[nextR][nextC] > babyShark.size
+							|| visited[nextR][nextC])
+						continue;
+					if (map[nextR][nextC] != 0 && map[nextR][nextC] < babyShark.size) {
+						
+						eatFish.add(new Fish(nextR, nextC, map[nextR][nextC]));
+						findMinDistance = true;
+					}
+					visited[nextR][nextC] = true;
+					queR.offer(nextR);
+					queC.offer(nextC);
 				}
-
+				size--;
 			}
+			if (findMinDistance)
+				break;
+		}
+
+		Collections.sort(eatFish);
+		if (!eatFish.isEmpty()) {
+			sizeList.poll();
+			map[babyShark.r][babyShark.c] = 0;
+			map[eatFish.get(0).r][eatFish.get(0).c] = 9;
+			babyShark.cnt++;
+			babyShark.r = eatFish.get(0).r;
+			babyShark.c = eatFish.get(0).c;
+			fishCnt--;
+			ans += distance;
+			if (babyShark.size == babyShark.cnt) {
+				babyShark.size++;
+				babyShark.cnt = 0;
+			}
+			eatFish.clear();
 		}
 	}
 }
@@ -107,8 +152,9 @@ class Fish implements Comparable<Fish> {
 
 	@Override
 	public int compareTo(Fish o) {
-		if (this.c == o.c)
-			return Integer.compare(this.r, o.r);
-		return Integer.compare(this.c, o.c);
+		if (this.r == o.r)
+			return Integer.compare(this.c, o.c);
+		return Integer.compare(this.r, o.r);
 	}
 }
+
