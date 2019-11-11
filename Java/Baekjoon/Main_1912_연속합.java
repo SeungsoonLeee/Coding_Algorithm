c9e4b47ac67d7995b1d214e7f882245a6bcc0caa
LSS <cjswosuny01@naver.com>
2019-11-11 14:23:07 +0900

    DP

--- Java/Baekjoon/Main_1912_연속합.java
+++ Java/Baekjoon/Main_1912_연속합.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_1912_연속합_이승순 {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
