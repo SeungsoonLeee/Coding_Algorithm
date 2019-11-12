74eeb56bff6a4aa42f458d8df8d12671bc30b5f0
LSS <cjswosuny01@naver.com>
2019-11-12 09:41:01 +0900

    코드 간략화

--- Java/Jungol/Main_2577_회전초밥_이승순.java
+++ Java/Jungol/Main_2577_회전초밥_이승순.java
@@ -18,38 +18,30 @@ public class Main_2577_회전초밥_이승순 {
 		c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
 		int[] dishes = new int[N];
 		int[] check = new int[d + 1];
-		int cnt = 0, ans = 0;
+		int cnt = 1, ans = 0;
 		Queue<Integer> ate = new LinkedList<Integer>();
 
 		for (int i = 0; i < N; i++) {
 			dishes[i] = Integer.parseInt(br.readLine());
 		}
-
+		
+		check[c]++;
 		for (int i = 0; i < k; i++) {
 			ate.offer(dishes[i]);
-			if (check[dishes[i]] == 0)
-				cnt++;
+			if (check[dishes[i]] == 0)	cnt++;
 			check[dishes[i]]++;
 		}
 
 		for (int i = k; i < N + k; i++) {
 			int temp = ate.poll();
-
+			
 			check[temp]--;
-			if (check[temp] == 0) {
-				cnt--;
-			}
+			if (check[temp] == 0)	cnt--;
 
-			if (check[dishes[i % N]] == 0) {
-				cnt++;
-			}
+			if (check[dishes[i % N]] == 0)	cnt++;
 			check[dishes[i % N]]++;
 
 			ate.offer(dishes[i % N]);
-			if (check[c] == 0) {
-				int tempCnt = cnt + 1;
-				ans = Math.max(ans, tempCnt);
-			}
 			ans = Math.max(ans, cnt);
 		}
 
